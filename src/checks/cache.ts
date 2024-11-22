acaf7ea614a014560f2d7bea824790ebb10ef86f
Josh Ghent <me@joshghent.com>
2024-11-22 23:09:59 +0000

    refactored the cache check code

--- src/checks/cache.ts
+++ src/checks/cache.ts
@@ -3,13 +3,41 @@ import { HealthCheck } from "./types.js";
 import {Redis} from "ioredis";
 import Memcached from "memcached";
 
+const checkMemcached = async (connection: string): Promise<boolean> => {
+  try {
+    const client = new Memcached(connection.replace("memcached://", ""));
+
+    await new Promise((resolve, reject) => {
+      client.version((err, result) => {
+        client.end();
+        if (err) reject(err);
+        else resolve(result);
+      });
+    });
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+const checkRedis = async (connection: string): Promise<boolean> => {
+  try {
+    const client = new Redis(connection);
+    await client.ping();
+    await client.quit();
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
 export const cacheConnection = async (config: Config): Promise<HealthCheck> => {
   const start = Date.now();
   if (!config.cache_connection) {
     return {
       componentName: "cache_connection",
       status: Status.fail,
-      message: "Cche connection string not configured",
+      message: "Cache connection string not configured",
       value: "N/A",
       time: Date.now() - start,
     };
@@ -18,39 +46,12 @@ export const cacheConnection = async (config: Config): Promise<HealthCheck> => {
   const isRedis = config.cache_connection.toLowerCase().startsWith("redis://");
   const isMemcached = config.cache_connection.toLowerCase().startsWith("memcached://");
 
-  try {
-    if (isRedis) {
-      const client = new Redis(config.cache_connection);
-      await client.ping();
-      await client.quit();
-
-      return {
-        componentName: "cache_connection",
-        status: Status.pass,
-        message: "Successfully connected to Redis",
-        value: "true",
-        time: Date.now() - start,
-      };
-    } else if (isMemcached) {
-      const client = new Memcached(config.cache_connection.replace("memcached://", ""));
-
-      await new Promise((resolve, reject) => {
-        client.version((err, result) => {
-          client.end();
-          if (err) reject(err);
-          else resolve(result);
-        });
-      });
-
-      return {
-        componentName: "cache_connection",
-        status: Status.pass,
-        message: "Successfully connected to Memcached",
-        value: "N/A",
-        time: Date.now() - start,
-      };
-    }
-
+  let result;
+  if (isRedis) {
+    result = await checkRedis(config.cache_connection);
+  } else if (isMemcached) {
+    result = await checkMemcached(config.cache_connection);
+  } else {
     return {
       componentName: "cache_connection",
       status: Status.fail,
@@ -58,13 +59,13 @@ export const cacheConnection = async (config: Config): Promise<HealthCheck> => {
       value: "N/A",
       time: Date.now() - start,
     };
-  } catch (error) {
-    return {
-      componentName: "cache_connection",
-      status: Status.fail,
-      message: `Failed to connect to cache`,
-      value: "N/A",
-      time: Date.now() - start,
-    };
+  }
+
+  return {
+    componentName: "cache_connection",
+    status: result ? Status.pass : Status.fail,
+    message: result ? "Cache connection successful" : "Cache connection failed",
+    value: result ? "true" : "false",
+    time: Date.now() - start,
   }
 };
